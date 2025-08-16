60e82bb5c8f2354269737609a7c7fb729c89c582
rainerpilarca18-a11y <rainerpilarca18@gmail.com>
2025-08-17 01:21:43 +0800

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,2 @@
+name = input("What's your name... ")
+print("Welcome To the Matrix",name)
---
+++ activity3.py
@@ -0,0 +1,2 @@
+print("Magandang Hapon \n\tBSIT-1C")
+print("The Quick Brown\n Fox Jumps Over \fThe Lazy Dog")
